c1572fad74ca6353634522acf40975d7e8d0c305
jjjaeunn <jjjaeunn@gmail.com>
2024-12-12 17:46:42 +0900

    주석 수정

--- 구현/15685.py
+++ 구현/15685.py
@@ -6,15 +6,15 @@ N = int(input())
 90도 회전 함수
 -------------
 parameters:
-direction - 시작점부터 끝점까지 차례대로 입력되어 있는 edge list
-return: 회전한 후의 인접 리스트
+direction - direction list
+return: 회전한 이후의 direction list
 '''
 def rotate_direction(direction:list) -> list:
     for i in range(len(direction)-1,-1,-1):
         direction.append((direction[i]+1)%4)
     return direction
 
-def move_forward(x,y,d):
+def move_forward(x,y,d) -> tuple:
     if d == 0:
         nx, ny = x+1, y
     if d == 1:
@@ -25,7 +25,7 @@ def move_forward(x,y,d):
         nx, ny = x, y+1
     return (nx,ny)
 
-def find_square(nodes:list)-> int:
+def find_square(nodes:list) -> int:
     count = 0
     for x, y in nodes:
         if (x+1, y) in nodes and (x, y+1) in nodes and (x+1, y+1) in nodes:
